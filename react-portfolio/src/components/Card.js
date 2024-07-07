83c17b7f433b7ac433fc373fd9a18ad1f2417fa8
mengzhuou <a1106983163@gmail.com>
2024-07-06 21:14:01 -0500

    Added a section to display your featured projects as cards in a grid fashion

--- react-portfolio/src/components/Card.js
+++ react-portfolio/src/components/Card.js
@@ -1,13 +1,34 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Heading, HStack, Image, Text, VStack, Flex, Spacer, Box, Grid, GridItem } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import './Card.css';
 
 const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  
+  // key={project.title}
+  //           title={project.title}
+  //           description={project.description}
+  //           imageSrc={project.getImageSrc()}
+  
+  return (
+    <HStack spacing={8}>
+      <Flex>
+        <Grid gap={4} className='gridStyle'>
+          <GridItem>
+            <Image src={imageSrc} className="imageItem"></Image>
+          </GridItem>
+          <GridItem>
+            <Text className="title">{title}</Text>
+            <Text mt={2} className="description">{description}</Text>
+          </GridItem>
+        </Grid>
+      </Flex>
+    </HStack>
+  );
 };
 
 export default Card;
